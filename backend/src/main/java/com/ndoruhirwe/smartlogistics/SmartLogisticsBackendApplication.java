838330a8944cc10cb9ab92462f78dedf119d726e
Ndoruhirwe.Jean <wndoru@gmail.com>
2026-07-09 12:37:45 +0200

    feat: initialize Spring Boot backend

---
+++ backend/src/main/java/com/ndoruhirwe/smartlogistics/SmartLogisticsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ndoruhirwe.smartlogistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartLogisticsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartLogisticsBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ndoruhirwe/smartlogistics/SmartLogisticsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ndoruhirwe.smartlogistics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartLogisticsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
