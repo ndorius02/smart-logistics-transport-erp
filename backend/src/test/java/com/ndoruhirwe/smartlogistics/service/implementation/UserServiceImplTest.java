01de13daaffb3e5a4eff2d15ebffb2e276f74f05
Ndoruhirwe.Jean <wndoru@gmail.com>
2026-07-29 15:25:42 +0200

    test(user): add unit tests for user service

--- backend/src/test/java/com/ndoruhirwe/smartlogistics/service/implementation/UserServiceImplTest.java
+++ backend/src/test/java/com/ndoruhirwe/smartlogistics/service/implementation/UserServiceImplTest.java
@@ -13,7 +13,6 @@ import com.ndoruhirwe.smartlogistics.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.password.PasswordEncoder;
